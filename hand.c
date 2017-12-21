d3189a4dbe6f6255e88d38c97678b7279e4a8940
kgashok <lifebalance@gmail.com>
2017-12-21 19:22:46 +0000

    Added podcast.txt and quiz files

--- hand.c
+++ hand.c
@@ -1,8 +1,24 @@
-#include <stdio.h> 
 
-int main() { 
-    // write your code below 
-    // for counting number of bytes in file hand.txt
-    
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    char fname[30];
+    if (argc > 1)
+        strcpy(fname, argv[1]);
+    else
+        strcpy(fname, "hand.txt");
+
+    FILE* fp = fopen(fname, "r");
+    if (fp == NULL) {
+        printf("File %s: Not available!\n", fname);
+        return -1;
+    }
+    int count = 0;
+    while (fgetc(fp) && !feof(fp) && ++count)
+        ;
+    printf("Number of bytes: %d\n", count);
+    fclose(fp);
     return 0;
 }
