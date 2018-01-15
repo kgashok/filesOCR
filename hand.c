0446611b5b68316ac9f70eefd3187e5f5e2add54
kgashok <lifebalance@gmail.com>
2018-01-15 16:45:48 +0000

    formatted hand.c and added junk.txt

--- hand.c
+++ hand.c
@@ -12,11 +12,9 @@
 int main(int argc, char* argv[])
 {
     char fname[30];
-    if (argc > 1)
-        strcpy(fname, argv[1]);
-    else
+    argc > 1? strcpy(fname, argv[1]): 
         strcpy(fname, "hand.txt");
-
+        
     FILE* fp = fopen(fname, "r");
     if (fp == NULL) {
         printf("File %s: Not available!\n", fname);
